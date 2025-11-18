30e1aecec94a5c89081ee9d1921a3cabc931fd14
Antony Remane <164482500+Antonyremay@users.noreply.github.com>
2025-11-18 23:03:47 +0530

    day_14

---
+++ pandas/pan4.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df=pd.read_csv(r"pandas\coffedata.csv")
+
+print(df[(df["coffee_name"]=="Latte") & (df["Monthsort"]==5)].head(10))
+print(df.shape)
+print(df.columns.to_list())
+print(df["coffee_name"].unique())
+print(df[df.isnull().any(axis=1)])
+print(df["Weekday"].isnull().sum())
+print(df[df["Weekday"].isnull()])
+print(df[df["Weekday"].isnull()])
+print(df.dropna())
